70dbc4f8f1eafed769299cb6ae0954e5e674de6e
gabrielbudke <gabrielbudke@gmail.com>
2020-01-15 15:10:12 -0300

    Criação do backend da aplicação

---
+++ backend/src/routes.js
@@ -0,0 +1,34 @@
+const { Router } = require("express");
+const axios = require("axios");
+const Dev = require("./models/Dev");
+
+const routes = Router();
+
+// Query params: request.query
+// Route params: request.params
+// Body params: request.body
+
+routes.post("/devs", async (request, response) => {
+  const { github_username, techs } = request.body;
+
+  const apiResponse = await axios.get(
+    `https://api.github.com/users/${github_username}`
+  );
+
+  const { name = login, bio, avatar_url } = apiResponse.data;
+
+  const techsArray = techs.split(",").map(tech => tech.trim());
+
+  const dev = await Dev.create({
+    github_username,
+    name,
+    avatar_url,
+    bio,
+    techs: techsArray
+  });
+
+  return response.json(dev);
+});
+
+// Exportando o objeto routes.
+module.exports = routes;
