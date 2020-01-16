1b55506086e03fc8b894926ed65857e4644edc8b
Gabriel Budke <gabrielbudke@gmail.com>
2020-01-15 23:48:22 -0300

    Criação do backend da aplicação - Cadastro da location do dev

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -9,7 +9,7 @@ const routes = Router();
 // Body params: request.body
 
 routes.post("/devs", async (request, response) => {
-  const { github_username, techs } = request.body;
+  const { github_username, techs, latitude, longitude } = request.body;
 
   const apiResponse = await axios.get(
     `https://api.github.com/users/${github_username}`
@@ -19,12 +19,18 @@ routes.post("/devs", async (request, response) => {
 
   const techsArray = techs.split(",").map(tech => tech.trim());
 
+  const location = {
+    type: "Point",
+    coordinates: [longitude, latitude]
+  };
+
   const dev = await Dev.create({
     github_username,
     name,
     avatar_url,
     bio,
-    techs: techsArray
+    techs: techsArray,
+    location
   });
 
   return response.json(dev);
