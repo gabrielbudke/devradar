e08a600eee2f564f4ed7ba89bca8856d9a4f454d
Gabriel Budke <gabrielbudke@gmail.com>
2020-01-16 00:34:27 -0300

    Criação do backend da aplicação - Listagem de devs

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -1,6 +1,5 @@
 const { Router } = require("express");
-const axios = require("axios");
-const Dev = require("./models/Dev");
+const DevController = require("./controllers/DevController");
 
 const routes = Router();
 
@@ -8,33 +7,8 @@ const routes = Router();
 // Route params: request.params
 // Body params: request.body
 
-routes.post("/devs", async (request, response) => {
-  const { github_username, techs, latitude, longitude } = request.body;
-
-  const apiResponse = await axios.get(
-    `https://api.github.com/users/${github_username}`
-  );
-
-  const { name = login, bio, avatar_url } = apiResponse.data;
-
-  const techsArray = techs.split(",").map(tech => tech.trim());
-
-  const location = {
-    type: "Point",
-    coordinates: [longitude, latitude]
-  };
-
-  const dev = await Dev.create({
-    github_username,
-    name,
-    avatar_url,
-    bio,
-    techs: techsArray,
-    location
-  });
-
-  return response.json(dev);
-});
+routes.get("/devs", DevController.index);
+routes.post("/devs", DevController.store);
 
 // Exportando o objeto routes.
 module.exports = routes;
